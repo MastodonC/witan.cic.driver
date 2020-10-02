3c82cfa0399483298de27f1745a7b0f21cca1cbd
Bruce Durling <bruce@mastodonc.com>
2020-10-02 11:56:32 +0100

    Make things more beautiful with functions and desctructuring
    See https://clojure.org/guides/destructuring#_namespaced_keywords

--- src/witan/cic/episodes/transformation_report.clj
+++ src/witan/cic/episodes/transformation_report.clj
@@ -30,12 +30,50 @@
    "original fields"
    "DECOM" "DEC" "PLACE" "LS" "CIN"])
 
+(defn format-episode [{::wce/keys [id dob sex
+                                   report-year period-id
+                                   reason-new-episode report-date ceased placement legal-status care-status]
+                       ::wcie/keys [file-name sheet-name row-index]
+                       :as episode}]
+  [id (str dob) sex
+   report-year period-id
+   (str reason-new-episode) (str report-date) (str ceased) placement legal-status care-status
+   file-name sheet-name row-index])
+
+(defn format-edit [{::wce/keys [command reason] :as edit}
+                   {new-report-year ::wce/report-year
+                    new-report-date ::wce/report-date
+                    new-ceased ::wce/ceased
+                    new-placement ::wce/placement
+                    new-legal-status ::wce/legal-status
+                    new-care-status ::wce/care-status
+                    ::wcie/keys [file-name sheet-name row-index]
+                    :as new}
+                   {previous-report-date ::wce/report-date
+                    previous-ceased ::wce/ceased
+                    previous-placement ::wce/placement
+                    previous-legal-status ::wce/legal-status
+                    previous-care-status ::wce/care-status
+                    :as previous}]
+  [(user-friendly-command-string command) reason
+   new-report-year
+   (str new-report-date) (str new-ceased) new-placement new-legal-status new-care-status
+   file-name sheet-name row-index
+   "Original" (str previous-report-date) (str previous-ceased) previous-placement previous-legal-status previous-care-status])
+
+(defn format-replacement [{::wce/keys [command reason]
+                           :as edit}
+                          {::wce/keys [report-year
+                                       report-date ceased placement legal-status care-status]
+                           ::wcie/keys [file-name sheet-name row-index]
+                           :as replacement}]
+  [(user-friendly-command-string command) reason
+   report-year
+   (str report-date) (str ceased) placement legal-status care-status
+   file-name sheet-name row-index])
+
 (defn format-row [episode]
-  (let [fields ((juxt ::wce/id (fn [e] (str (::wce/dob e))) ::wce/sex
-                      ::wce/report-year ::wce/period-id
-                      (fn [e] (str (::wce/reason-new-episode e))) (fn [e] (str (::wce/report-date e))) (fn [e] (str (::wce/ceased e))) ::wce/placement ::wce/legal-status ::wce/care-status
-                      ::wcie/file-name ::wcie/sheet-name ::wcie/row-index)
-                episode)
+  (let [fields (format-episode episode)
         edit (or
               (some #(when (= (::wce/command %) :remove) %) (::wce/edit episode))
               (first (::wce/edit episode)))]
@@ -43,19 +81,11 @@
       (::wce/new edit)
       (let [new (::wce/new edit)
             previous (::wce/previous edit)]
-        (into fields
-              [(user-friendly-command-string (::wce/command edit)) (::wce/reason edit)
-               (::wce/report-year new)
-               (str (::wce/report-date new)) (str (::wce/ceased new)) (::wce/placement new) (::wce/legal-status new) (::wce/care-status new)
-               (::wcie/file-name new) (::wcie/sheet-name new) (::wcie/row-index new)
-               "Original" (str (::wce/report-date previous)) (str (::wce/ceased previous)) (::wce/placement previous) (::wce/legal-status previous) (::wce/care-status previous)]))
+        (into fields (format-edit edit new previous)))
 
       (::wce/replacement edit)
       (let [replacement (::wce/replacement edit)]
-        (into fields [(user-friendly-command-string (::wce/command edit)) (::wce/reason edit)
-                      (::wce/report-year replacement)
-                      (str (::wce/report-date replacement)) (str (::wce/ceased replacement)) (::wce/placement replacement) (::wce/legal-status replacement) (::wce/care-status replacement)
-                      (::wcie/file-name replacement) (::wcie/sheet-name replacement) (::wcie/row-index replacement)]))
+        (into fields (format-replacement edit replacement)))
 
       :else
       (into fields [(user-friendly-command-string (::wce/command edit)) (::wce/reason edit)]))))
@@ -73,30 +103,17 @@
                   (x/into []
                           format-rows-xf
                           episodes))
-    (xl/save-workbook! out-file template))
+    (xl/save-workbook! out-file template)))
+
+(comment
+
+  ;; old way to save ->excel
   #_(xl/save-workbook!
      out-file
      (xl/create-workbook
       "Transformation Report"
       (x/into [header]
               format-rows-xf
-              episodes))))
+              episodes)))
 
-(comment
-
-  (def wb (xl/load-workbook "suffolk-transformation-report-20201001.xlsx"))
-
-  (->> wb
-       (xl/add-rows! "Transformation Report"
-                     [["foo" "bar"]
-                      [1 2]])
-       (xl/save-workbook! "test.xlsx"))
-
-  (let [wb (xl/load-workbook "suffolk-transformation-report-20201001.xlsx")
-        ]
-    (xl/add-rows! sheet [["foo" "bar"]
-                         [1 2]])
-    (xl/save-workbook! "test.xlsx" wb))
-  
-  
   )
